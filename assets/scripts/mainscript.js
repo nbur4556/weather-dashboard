283edf67529ee4e1daf4705a13b407ca687b979d
nbur4556 <nbur4556@gmail.com>
2020-08-23 12:29:35 -0500

    cleaned up code with es6 string building

--- assets/scripts/mainscript.js
+++ assets/scripts/mainscript.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     //Open Weather API Variables
     const openWeatherKey = 'd4e1e14217e539534a82f3014cd52789';
-    let cityName = 'Austin';
-    let apiURL = ('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + openWeatherKey);
+    let cityName = 'Dallas';
+    let apiURL = (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openWeatherKey}`);
 
     $.ajax({
         url: apiURL,
@@ -16,24 +16,24 @@ $(document).ready(function () {
 
     //Get icon from open weather maps icon url
     /*Icons include:
-        01d == 'Clear Sky'
-        02d == 'Few Clouds'
-        03d == 'Scattered Clouds'
-        04d == 'Broken Clouds'
-        09d == 'Shower Rain'
-        10d == 'Rain'
-        11d == 'Thunderstorm'
-        13d == 'Snow'
-        50d == 'Mist'
+        01d || 01n == 'Clear Sky'
+        02d || 02n == 'Few Clouds'
+        03d || 03n == 'Scattered Clouds'
+        04d || 04n == 'Broken Clouds'
+        09d || 09n == 'Shower Rain'
+        10d || 10n == 'Rain'
+        11d || 11n == 'Thunderstorm'
+        13d || 13n == 'Snow'
+        50d || 50n == 'Mist'
     */
     function getIconUrl(iconId, size = 'small') {
         let iconUrl = '';
 
         if (size == 'small') {
-            iconUrl = ('http://openweathermap.org/img/wn/' + iconId + '@2x.png')
+            iconUrl = (`http://openweathermap.org/img/wn/${iconId}@2x.png`)
         }
         else {
-            iconUrl = ('http://openweathermap.org/img/wn/' + iconId + '@4x.png')
+            iconUrl = (`http://openweathermap.org/img/wn/${iconId}@4x.png`)
         }
 
         return iconUrl
