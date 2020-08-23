79466620a2f43d6edd4b5f32801e0c9c25f01543
nbur4556 <nbur4556@gmail.com>
2020-08-23 12:18:55 -0500

    setup ajax call to retrieve information from open weather map api

--- assets/scripts/mainscript.js
+++ assets/scripts/mainscript.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-    $('button').click(function () {
-        alert('hello world');
+    //Open Weather API Variables
+    const openWeatherKey = 'd4e1e14217e539534a82f3014cd52789';
+    let cityName = 'Austin';
+    let apiURL = ('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + openWeatherKey);
+
+    $.ajax({
+        url: apiURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.weather[0].icon);
     });
 });
