ca7430f6517daebe52e412384954f4e39256a847
nbur4556 <nbur4556@gmail.com>
2020-08-23 12:20:50 -0500

    set up function to retrieve icon url from the open weather maps icons

--- assets/scripts/mainscript.js
+++ assets/scripts/mainscript.js
@@ -9,6 +9,34 @@ $(document).ready(function () {
         method: 'GET'
     }).then(function (response) {
         console.log(response);
-        console.log(response.weather[0].icon);
+
+        console.log(getIconUrl('11d'));
+        console.log(getIconUrl(response.weather[0].icon));
     });
+
+    //Get icon from open weather maps icon url
+    /*Icons include:
+        01d == 'Clear Sky'
+        02d == 'Few Clouds'
+        03d == 'Scattered Clouds'
+        04d == 'Broken Clouds'
+        09d == 'Shower Rain'
+        10d == 'Rain'
+        11d == 'Thunderstorm'
+        13d == 'Snow'
+        50d == 'Mist'
+    */
+    function getIconUrl(iconId, size = 'small') {
+        let iconUrl = '';
+
+        if (size == 'small') {
+            iconUrl = ('http://openweathermap.org/img/wn/' + iconId + '@2x.png')
+        }
+        else {
+            iconUrl = ('http://openweathermap.org/img/wn/' + iconId + '@4x.png')
+        }
+
+        return iconUrl
+    }
+
 });
